fd2e5b39b01f27846ed5dad8c13d5647708d2a66
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-12 04:21:27 +0200

    Change PATH_INFO to REQUEST_URI

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-$path = $_SERVER['PATH_INFO']; // REQUEST_URI
+//$path = $_SERVER['PATH_INFO']; // @note Не работает в Nginx конфиге для Laravel. Пустое значение PATH_INFO
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($path === '/help') {
     require_once './help.php';
