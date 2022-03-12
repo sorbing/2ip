ab398af718bbced8cbce367615a0ec7b98ee319e
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-12 04:33:27 +0200

    Update

--- public/help.php
+++ public/help.php
@@ -1,5 +1,8 @@
 <?php header('Content-Type: text/plain'); ?>
 curl -s 2ip.fun
-curl -s 2ip.fun/header
+curl -s 2ip.fun/headers
+curl -s 2ip.fun/h
 curl -s 2ip.fun/info[?ip=x.x.x.x&o=ip,country_name,city,asn.name&json&key=...] - ipdata.co information
+curl -s 2ip.fun/i
 curl -s 2ip.fun/quality[?ip=x.x.x.x&key=...] - ipdata.co + ipqualityscore.com compact information
+curl -s 2ip.fun/q
--- readme.md
+++ readme.md
@@ -15,3 +15,10 @@ curl 2ip.fun/header
 curl -s 2ip.fun/info[?ip=x.x.x.x&o=ip,country_name,city,asn.name&json&key=...] - ipdata.co information
 curl -s 2ip.fun/quality[?ip=x.x.x.x&key=...] - ipdata.co + ipqualityscore.com compact information
 ```
+
+## Deploy on your server
+
+```shell
+git clone git://github.com/sorbing/2ip.git .
+cp config.example.php config.php
+```
