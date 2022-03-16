43017e19ba6a6e2999490ce3d5d02fafb3f111c6
Stanislav Butsenko <svbutsenko@gmail.com>
2022-03-16 22:40:57 +0200

    public/deploy.php

--- public/deploy.php
+++ public/deploy.php
@@ -1,18 +1,29 @@
 <?php
 
+header('Content-Type: text/plain; charset=utf-8');
+
 $config = require_once __DIR__ . '/../config.php';
 
 $key = $_REQUEST['deploy-key'] ?? '';
 
 if (!$key || $key !== $config->APP_DEPLOY_KEY) {
     header('HTTP/1.0 403 Forbidden');
-    die("Forbidden");
+    echo "Forbidden\n";
+    exit(1);
 }
 
-echo "@todo Run deploy..\n";
+//$body = file_get_contents('php://input');
+//logDeployRequest($body);
+//$json = json_decode($body);
+////$pushBranch = $json->push->changes[0]->new->name;
 
-// @todo
-//git checkout -- .
-//git pull -X theirs
-//$cmd = sprintf('cd ../ && git pull -X theirs');
-//exec($cmd);
+// @todo Проверить, что процесс деплоя в данный момент не запущен!
+// @todo Проверить, что разрешено запускать deploy.sh от юзера www-data:
+//       sudo visudo -f /etc/sudoers.d/www-data
+
+echo date('Y-m-d H:i:s') . "\n";
+echo "Run deploy..\n";
+$command = sprintf('cd ../ && sudo -Hu tuner git pull -X theirs 2>&1');
+exec($command, $output, $retCode);
+echo implode("\n", $output) . "\n";
+echo "Exit code: $retCode";
